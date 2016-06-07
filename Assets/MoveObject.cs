020450ca6a1c0e8b8ce2e9813a2c49e80d62e193
enotesupa <abingdon.girls.school@hotmail.co.jp>
2016-06-08 00:06:53 +0900

    enabled two-person operating

--- Assets/MoveObject.cs
+++ Assets/MoveObject.cs
@@ -13,27 +13,24 @@ public class MoveObject : MonoBehaviour {
 	public GameObject origin;
 	private GameObject[,] objects;
 
-	public int cols = 10;
-	public int rows = 8;
-	public int array_size;
-	public int expansion_rate = 1;
+	public int cols = 40;
+	public int rows = 40;
+	public float expansion_rate = 2f;
 
     public int points = 0;
     public GameObject picture;
     private GameObject copied_picture;
 
-	//private Rigidbody2D rbody;
-
 	// Use this for initialization
 	void Start () {
-		//rbody = GetComponent<Rigidbody2D>();
-		array_size = cols * rows ;
-		vecmap = new vecbool[array_size,array_size];
+        cols = rows;
+		vecmap = new vecbool[cols, rows];
 		objects = new GameObject[cols, rows];
 		for (int i = 0; i < cols; i++) {
 			for (int j = 0; j < rows; j++) {
 				vecmap[i,j].is_stepped = false;
-				vecmap[i,j].pos = new Vector2(-(float)cols/20f +(float)i, rows/2f - (float)j );
+				vecmap[i,j].pos = new Vector2(-(float)cols/cols/2f +(float)i/ expansion_rate,
+                    rows/ expansion_rate - (float)j/ expansion_rate);
 				objects[i,j] = Instantiate(origin) as GameObject;
 				objects[i,j].transform.position = new Vector3(vecmap[i,j].pos.x, vecmap[i,j].pos.y, transform.position.z+1);
 				objects[i,j].name = i+" "+j;
@@ -49,36 +46,8 @@ public class MoveObject : MonoBehaviour {
             transform.localScale.z);
     }
 
-//	void check_footprint(){
-//		float min = (cols * cols) + (rows * rows);
-//		for (int i = 0; i < array_size; i++) {
-//			for (int j = 0; j < array_size; j++) {
-//				float tmp_dis = (vecmap[i,j].pos.x) * (vecmap[i,j].pos.x)
-//					+ (vecmap[i,j].pos.y) * (vecmap[i,j].pos.y);
-//				if(tmp_dis < min){
-//					min = tmp_dis;
-//				}
-//			}
-//		}
-//		for (int i = 0; i < array_size; i++) {
-//			for (int j = 0; j < array_size; j++) {
-//				float tmp_dis = (vecmap[i,j].pos.x) * (vecmap[i,j].pos.x)
-//					+ (vecmap[i,j].pos.y) * (vecmap[i,j].pos.y);
-//				if(tmp_dis == min){
-//					if(!vecmap[i,j].is_stepped){
-//						Debug.Log(i+", "+j+" ("+vecmap[i,j].pos.x+", "+vecmap[i,j].pos.y+") stepped!");
-//					}
-//					vecmap[i,j].is_stepped = true;
-//					return;
-//				}
-//			}
-//		}
-//	}
-
 	void OnTriggerEnter2D(Collider2D other) {
-		//Debug.Log(other.transform.position.x+", "+other.transform.position.y+" :"+other.name);
 		string[] strarg = other.name.Split(' ');
-		//Debug.Log(vecmap[int.Parse(strarg[0]),int.Parse(strarg[1])].is_stepped+"");
         if(vecmap[int.Parse(strarg[0]), int.Parse(strarg[1])].is_stepped == false)
         {
             points++;
@@ -89,24 +58,16 @@ public class MoveObject : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			//rbody.MovePosition(new Vector3(transform.position.x,transform.position.y+0.1f,transform.position.z));
 			transform.position = new Vector3(transform.position.x,transform.position.y+0.1f,transform.position.z);
-			//check_footprint ();
 		}
 		if (Input.GetKey (KeyCode.DownArrow)) {
-			//rbody.MovePosition(new Vector3(transform.position.x,transform.position.y-0.1f,transform.position.z));
 			transform.position = new Vector3(transform.position.x,transform.position.y-0.1f,transform.position.z);
-			//check_footprint ();			
 		}
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			//rbody.MovePosition(new Vector3(transform.position.x+0.1f,transform.position.y,transform.position.z));
 			transform.position = new Vector3(transform.position.x+0.1f,transform.position.y,transform.position.z);
-			//check_footprint ();			
 		}
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			//rbody.MovePosition(new Vector3(transform.position.x-0.1f,transform.position.y,transform.position.z));
 			transform.position = new Vector3(transform.position.x-0.1f,transform.position.y,transform.position.z);
-			//check_footprint ();			
 		}
 	}
 }
